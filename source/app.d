331ae12d279cf8dd3ea107dcd10199061717d2e3
Dylan Knutson <tcdknutson@gmail.com>
2013-12-28 22:49:12 -0800

    mixinify common methods

--- source/app.d
+++ source/app.d
@@ -20,7 +20,7 @@ void main()
 	/**
 	 * Passing functions as handlers
 	 */
-	router.push("/foo/bar", {
+	router.get("/foo/bar", {
 		writeln("/foo/bar was called");
 	});
 
@@ -28,7 +28,7 @@ void main()
 	 * Passing functions as handlers, which take a
 	 * string[string] containing the params hash
 	 */
-	router.push("/job/:name/:occupation", (params) {
+	router.get("/job/:name/:occupation", (params) {
 		writefln("%s is a %s", params["name"], params["occupation"]);
 	});
 
@@ -36,7 +36,7 @@ void main()
 	 * Optional route parts
 	 * This will match on /user, or /user/bob
 	 */
-	router.push("/user/:name?", (params) {
+	router.get("/user/:name?", (params) {
 
 		auto name = "name" in params;
 		if(name is null)
@@ -54,7 +54,7 @@ void main()
 	 * Delegate support
 	 */
 	int i = 0;
-	router.push("/callme", {
+	router.get("/callme", {
 
 		i++;
 		writefln("Been called %d times", i);
@@ -65,7 +65,7 @@ void main()
 	 * Delegate with param support
 	 */
 	string[] seen_names;
-	router.push("/addname/:name", (params) {
+	router.get("/addname/:name", (params) {
 
 		seen_names ~= params["name"];
 		writeln("Seen names: ", seen_names);
@@ -76,9 +76,9 @@ void main()
 	 * Push method chaining
 	 */
 	router
-	.push("/chained/a", () { writeln("Matched a"); })
-	.push("/chained/b", () { writeln("Matched b"); })
-	.push("/chained/:o", (params) {
+	.get("/chained/a", () { writeln("Matched a"); })
+	.get("/chained/b", () { writeln("Matched b"); })
+	.get("/chained/:o", (params) {
 		writeln("Matched other: ", params["o"]);
 	});
 
@@ -92,7 +92,7 @@ void main()
 		if(line is null || !line.chomp.length)
 			break;
 
-		router.match(line.chomp.strip);
+		router.match(line.chomp.strip, Method.Get);
 	}
 }
 
@@ -108,13 +108,13 @@ void bench()
 
 	foreach(pattern; ["/foo", "/:name", "/foo/:bar/:baz"])
 	{
-		r1.push(new SplitterRoute(pattern), &nil);
-		r2.push(new RegexRoute(pattern), &nil);
+		r1.get(new SplitterRoute(pattern), &nil);
+		r2.get(new RegexRoute(pattern), &nil);
 	}
 
 	void doMatch(ref Router r)
 	{
-		r.match("/baz");
+		r.match("/baz", Method.Get);
 	}
 
 	void testr1()
--- source/d_router/router.d
+++ source/d_router/router.d
@@ -2,6 +2,7 @@ module d_router.router;
 
 import
 	std.stdio,
+	std.array,
 	std.regex,
 	std.string,
 	std.algorithm,
@@ -27,6 +28,7 @@ private
 
 	struct RouteCallbackPair
 	{
+		Method method;
 		Route route;
 		union
 		{
@@ -38,9 +40,10 @@ private
 		bool is_delegate;
 		bool takes_params;
 
-		this(CBType)(Route r, CBType cb)
+		this(CBType)(Method m, Route r, CBType cb)
 		if(isCallback!CBType)
 		{
+			method = m;
 			route = r;
 
 			static if(is(CBType == delegate))
@@ -71,6 +74,14 @@ private
 	}
 }
 
+enum Method
+{
+	Get    = 1,
+	Post   = 2,
+	Put    = 4,
+	Delete = 8
+}
+
 struct Router
 {
 private:
@@ -84,44 +95,100 @@ private:
 			return new SplitterRoute(route_pattern);
 	}
 
+	template BuildHttpMethods(string ident, alias method)
+	{
+		enum BuildHttpMethods =
+		q{
+			ref Router __ident(string pattern, CB_Function callback)
+			{
+				return define(__method, pattern, callback);
+			}
+			ref Router __ident(string pattern, CB_Function_Params callback)
+			{
+				return define(__method, pattern, callback);
+			}
+			ref Router __ident(string pattern, CB_Delegate callback)
+			{
+				return define(__method, pattern, callback);
+			}
+			ref Router __ident(string pattern, CB_Delegate_Params callback)
+			{
+				return define(__method, pattern, callback);
+			}
+
+			ref Router __ident(Route route, CB_Function callback)
+			{
+				return define(__method, route, callback);
+			}
+			ref Router __ident(Route route, CB_Function_Params callback)
+			{
+				return define(__method, route, callback);
+			}
+			ref Router __ident(Route route, CB_Delegate callback)
+			{
+				return define(__method, route, callback);
+			}
+			ref Router __ident(Route route, CB_Delegate_Params callback)
+			{
+				return define(__method, route, callback);
+			}
+		}
+		.replace("__ident", ident)
+		.replace("__method", method.stringof);
+	}
+
+	template SpecializedDefine(string cb_type)
+	{
+		enum SpecializedDefine =
+		q{
+			ref Router define(
+				Method method,
+				string route_pattern,
+				%s callback)
+			{
+				return define(method, routeFor(route_pattern), callback);
+			}
+		}.format(cb_type);
+	}
+
 public:
 
+	// DMD bugs mean mixin templates can't
+	// be used to generate this, so do this hackery to
+	// generate all the variants of get, post, put, and delete
+	mixin(BuildHttpMethods!("get", Method.Get));
+	mixin(BuildHttpMethods!("post", Method.Post));
+	mixin(BuildHttpMethods!("put", Method.Put));
+	mixin(BuildHttpMethods!("_delete", Method.Delete));
+
+	// Parameter types are explicitly stated to allow type
+	// inference on lambdas passed to get, post, define, etc
 	// Order is important here: Prefer function type
 	// callbacks above delegates in order to not
 	// allocate a delegate's environment on each invocation
-	ref Router push(string route_pattern, CB_Function callback)
-	{
-		return push(routeFor(route_pattern), callback);
-	}
+	mixin(SpecializedDefine!"CB_Function");
+	mixin(SpecializedDefine!"CB_Function_Params");
+	mixin(SpecializedDefine!"CB_Delegate");
+	mixin(SpecializedDefine!"CB_Delegate_Params");
 
-	ref Router push(string route_pattern, CB_Function_Params callback)
-	{
-		return push(routeFor(route_pattern), callback);
-	}
-
-	ref Router push(string route_pattern, CB_Delegate callback)
-	{
-		return push(routeFor(route_pattern), callback);
-	}
-
-	ref Router push(string route_pattern, CB_Delegate_Params callback)
-	{
-		return push(routeFor(route_pattern), callback);
-	}
-
-	ref Router push(CBType)(Route route, CBType callback)
+	ref Router define(CBType)(
+		Method method,
+		Route route,
+		CBType callback)
 	if(isCallback!CBType)
 	{
-		routes ~= RouteCallbackPair(route, callback);
+		routes ~= RouteCallbackPair(method, route, callback);
 		return this;
 	}
 
-	bool match(string pattern)
+	bool match(string pattern, Method method)
 	{
 		string[string] matched_params;
 		foreach(ref pair; routes)
 		{
-			if(pair.route.matches(pattern, matched_params))
+			if(
+				(pair.method & method) != 0 &&
+				pair.route.matches(pattern, matched_params))
 			{
 				if(pair.is_delegate)
 				{
@@ -151,15 +218,15 @@ unittest
 	auto r = Router();
 	bool hit = false;
 
-	r.push("/foo", () { hit = true; });
-	r.push("/bar", () { hit = false; });
+	r.get("/foo", () { hit = true; });
+	r.get("/bar", () { hit = false; });
 
-	assert(r.match("/foo"));
+	assert(r.match("/foo", Method.Get));
 	assert(hit);
 
-	assert(!r.match("/asdf"));
-	assert(!r.match("/foo/bar"));
-	assert(!r.match("/bar/foo"));
+	assert(!r.match("/asdf", Method.Get));
+	assert(!r.match("/foo/bar", Method.Get));
+	assert(!r.match("/bar/foo", Method.Get));
 }
 
 unittest
@@ -169,22 +236,42 @@ unittest
 	string hit;
 
 	r
-	.push("/a/a", { hit = "aye"; })
-	.push("/a/b", { hit = "bee"; })
-	.push("/a/:o", (p) { hit = p["o"]; });
+	.get("/a/a", { hit = "aye"; })
+	.get("/a/b", { hit = "bee"; })
+	.get("/a/:o", (p) { hit = p["o"]; });
 
-	assert(!r.match("/a/"));
-	assert(!r.match("/a"));
+	assert(!r.match("/a/", Method.Get));
+	assert(!r.match("/a", Method.Get));
 
-	assert(r.match("/a/a"));
+	assert(r.match("/a/a", Method.Get));
 	assert(hit == "aye");
 
-	assert(r.match("/a/b"));
-	assert(hit == "bee");
+	assert(r.match("/a/b", Method.Get));
+	assert(hit == "bee", "Hit was: " ~ hit);
 
-	assert(r.match("/a/foo"));
+	assert(r.match("/a/foo", Method.Get));
 	assert(hit == "foo");
 
-	assert(r.match("/a/z"));
+	assert(r.match("/a/z", Method.Get));
 	assert(hit == "z");
+}
+
+unittest
+{
+	auto r = Router();
+
+	with(Method)
+	{
+		r.define(Get|Post, "/a", {});
+		assert(r.match("/a", Get));
+		assert(r.match("/a", Post));
+	}
+
+	with(Method)
+	{
+		r.define(Get, "/b", {});
+		assert(r.match("/b", Get|Post));
+		assert(!r.match("/b", Post));
+	}
+
 }
